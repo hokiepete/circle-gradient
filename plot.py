269c747857134de0a69ce66d4b4ed7cb6b27a565
Peter <pnolan1986@gmail.com>
2018-06-14 10:54:52 -0400

    updates

--- plot.py
+++ plot.py
@@ -23,8 +23,8 @@ with hp.File('850mb_300m_10min_NAM_Rhodot_Origin_t=0-215hrs_Sept2017.hdf5','r') 
 #    data.close()
 
 with hp.File('hunterdata.mat','r') as data:
-    rhodotx = data['rhodot'][:]
-    tx = data['timeout'][:]
+    rhodotx = data['rhodot'][:].squeeze()
+    tx = data['timeout'][:].squeeze()
 
 #rhodot=f['rhodot']
 '''
@@ -35,14 +35,34 @@ with hp.File('hunterdata.mat','r') as data:
 #    data.close()
 #plt.plot(t,rhodot_origin,color='b')
 #plt.plot(t,rhodot[:,125,130],color='r')
+
 tcku = sint.splrep(t, rhodot, s=0)
 rwant= sint.splev(tx, tcku, der=0)
-plt.close()
+#
+import pandas as pd
+from pandas.plotting import scatter_matrix
+A = pd.DataFrame(np.transpose([rwant,rhodotx]),columns=['rho','hun'])
+print A.corr()
 fig = plt.figure(1)
+scatter_matrix(A)
+
+'''
+cor = np.correlate(rwant.squeeze(),rhodotx.squeeze(),mode='full')
+print cor
+
+del tx, t, rhodot, tcku
+from scipy.stats import linregress
+show = linregress(rwant, rhodotx)
+print show
+'''
+
+plt.close('all')
+
+fig = plt.figure(2)
 #ax2=plt.plot((po-46)*3600,prhodot,color='k',label="Peter's virtual flight 5x/hr")
 #ax4=plt.plot((to-46)*7200,htrhodot,color='b',label="Peter's virtual flight 10x/hr")
-ax1=plt.plot(tx,rhodotx,color='y',label="Hunter's flight simulation")
-ax4=plt.plot(tx,rwant,color='b',label="Rhodot")
+ax1=plt.plot(tx,rhodotx*3600,color='y',label="Hunter's flight simulation")
+ax4=plt.plot(tx,rwant*3600,color='b',label="Rhodot")
 
 #ax3=plt.plot(t,rhodot,color='r',label="Rhodot")
 
@@ -53,5 +73,5 @@ rwant -= rwant.min()
 rwant = rwant/rwant.max()
 rhodotx -= rhodotx.min()
 rhodotx = rhodotx/rhodotx.max()
-fig = plt.figure(2)
+fig = plt.figure(3)
 ax1 = plt.scatter(rwant,rhodotx)
