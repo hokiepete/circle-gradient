7ffaae9139ccfd6494e2703d5035fc35c9660756
Peter <pnolan1986@gmail.com>
2018-11-30 11:14:19 -0500

    removals

--- animate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#This script converts a series of images to a video using ffmpeg
-ffmpeg -f image2 -framerate 10 -i 'Kentland_lcs_%04d.tif' -pix_fmt yuv420p -vf scale=1280:-2 'LCS.mp4'
-ffmpeg -f image2 -framerate 10 -i 'quiver%04d.png' -pix_fmt yuv420p -vf scale=1280:-2 'vec.mp4'
